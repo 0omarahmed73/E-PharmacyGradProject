07316b6bba8c18d5c98aa4a05bf0d2781d4a7437
Omar Ahmed <0omarahmed73@gmail.com>
2024-06-05 07:57:38 +0300

    done

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,10 +2,12 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
   app.use(
-    '/api', // Specify the path you want to proxy
+    '/auth/login', // Specify the path you want to proxy
     createProxyMiddleware({
       target: 'https://pharmacy-1xjk.onrender.com', // Specify the backend server URL
       changeOrigin: true,
     })
+    ,
+    
   );
 };
