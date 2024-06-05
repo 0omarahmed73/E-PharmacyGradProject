6bef4bfff953e9064dbc815aebb23e6fa36e1ee9
Omar Ahmed <0omarahmed73@gmail.com>
2024-06-05 07:58:52 +0300

    chore: Update setupProxy.js to add proxy configurations

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,13 +1,29 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
-  app.use(
-    '/auth/login', // Specify the path you want to proxy
-    createProxyMiddleware({
-      target: 'https://pharmacy-1xjk.onrender.com', // Specify the backend server URL
+  // Define an array of proxy configurations
+  const proxies = [
+    {
+      path: '/auth/login',
+      target: 'https://backend1.example.com',
       changeOrigin: true,
-    })
-    ,
-    
-  );
+    },
+    {
+      path: '/pharmacy',
+      target: 'https://backend2.example.com',
+      changeOrigin: true,
+    },
+    // Add more proxy configurations as needed
+  ];
+
+  // Iterate over the proxy configurations and set up proxies
+  proxies.forEach(({ path, target, changeOrigin }) => {
+    app.use(
+      path,
+      createProxyMiddleware({
+        target,
+        changeOrigin,
+      })
+    );
+  });
 };
